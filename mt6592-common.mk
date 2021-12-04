5291c9a5ce489a2235a08e3ced9453a14e3cfc67
R0rt1z2 <rogerortizleal@gmail.com>
2021-12-04 16:34:59 +0100

    mt6592-common: add stock configs
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- mt6592-common.mk
+++ mt6592-common.mk
@@ -29,6 +29,43 @@ PRODUCT_PACKAGES += \
     ueventd.mt6592.rc \
     init.recovery.mt6592.rc
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(COMMON_PATH)/configs/media/player.cfg:system/etc/player.cfg
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(COMMON_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/bluetooth/auto_pair_blacklist.conf:system/etc/bluetooth/auto_pair_blacklist.conf \
+    $(COMMON_PATH)/configs/bluetooth/auto_pair_devlist.conf:system/etc/bluetooth/auto_pair_devlist.conf \
+    $(COMMON_PATH)/configs/bluetooth/btconfig.xml:system/etc/bluetooth/btconfig.xml \
+    $(COMMON_PATH)/configs/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
+    $(COMMON_PATH)/configs/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf
+
+# RIL
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/ril/apns-conf.xml:system/etc/apns-conf.xml \
+    $(COMMON_PATH)/configs/ril/spn-conf.xml:system/etc/spn-conf.xml \
+    $(COMMON_PATH)/configs/ril/virtual-spn-conf-by-efgid1.xml:system/etc/virtual-spn-conf-by-efgid1.xml \
+    $(COMMON_PATH)/configs/ril/virtual-spn-conf-by-efspn.xml:system/etc/virtual-spn-conf-by-efspn.xml \
+    $(COMMON_PATH)/configs/ril/virtual-spn-conf-by-efpnn.xml:system/etc/virtual-spn-conf-by-efpnn.xml \
+    $(COMMON_PATH)/configs/ril/virtual-spn-conf-by-imsi.xml:system/etc/virtual-spn-conf-by-imsi.xml    
+
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(COMMON_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# GPS
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/gps/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
+
 # Override GMS Client ID
 PRODUCT_GMS_CLIENTID_BASE := android-bq
 
