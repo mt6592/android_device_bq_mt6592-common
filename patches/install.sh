6e3417c7a33d590be1a582982c80707863301771
Wang Xuerui <idontknw.wang@gmail.com>
2021-12-05 12:00:41 +0100

    mt6592-common: add system_core patch
    * Enforces the xlog logging hooks.
    * [TMP]: Disables SELinux (Permissive).
    * Increases the ENV length (for mtk-ril).
    * Disables AOSP rild service (causes problems with MTK's impl).
    * Fixes boot mode for offline charging.
    * Hardcodes deprecated callstack symbols.

---
+++ patches/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo $1
+rootdirectory="$PWD"
+
+dirs="system/core"
+
+# red + nocolor
+RED='\033[0;31m'
+NC='\033[0m'
+
+for dir in $dirs ; do
+    cd $rootdirectory
+    cd $dir
+    echo "Applying ${dir} patches...\n"
+    git apply -v $rootdirectory/device/bq/mt6592-common/patches/$dir/*.patch
+done
+
+echo -e "Done !\n"
+cd $rootdirectory
